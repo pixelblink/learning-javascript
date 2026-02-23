9f15460f69b0e52cc82df7e2ddcd0da32f7face7
pixelblink <chandandhare98@gmail.com>
2026-02-23 23:53:36 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,11 @@
+// const pi=3.1415
+
+// pi=3.1416
+
+// console.log(pi);
+
+// var j = Number(prompt("input dedo"))
+// var k = parseInt(prompt("input dedo"))
+// var l = parseFloat(prompt("input dedo"))
+// console.log(j+k+l);
+ 
