f27b7912a923f4357ccc140f82685b895159ddce
pixelblink <chandandhare98@gmail.com>
2026-03-01 12:57:25 +0530

    day4

---
+++ 4.js
@@ -0,0 +1,25 @@
+// types of for loops
+// ===========for of===========
+//  for of loop is used to acces the elements of an arry without any indexing needing
+
+
+// let arey=[1,2,3,4,5,6,6,6,]
+
+// for (let storage of arey) {
+//     console.log(storage);
+    
+// }
+
+// Q1. make an arry of 1 to 10 number and print square of even numbers using for of loop
+
+// let arey=[1,2,3,4,5,6,7,8,9,10]
+
+// for (const storage of arey) {
+
+//     if(storage%2==0){
+//         console.log(storage*storage);
+//     }
+// }
+
+
+
