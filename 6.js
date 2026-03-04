768a9a045ffedca390701bcbf3b323259851d6a4
pixelblink <chandandhare98@gmail.com>
2026-03-04 15:24:36 +0530

    day6

---
+++ 6.js
@@ -0,0 +1,76 @@
+// Function is a block of code or set of instructions which is defined once and then can be called however many times
+// Function declaration, function defination and function calling are 3 main points which are required to make and run a function succesfully
+
+// Types of functions=>> 
+// ->User Defined 
+//      Types of user defined functions
+//          without argument and no return value
+//          with argument and no return value
+//          without argument and with return value
+//          with argument and with return value
+// ->Pre Defined
+
+
+
+
+
+// function IdentifierBolteHaiIssko("PARAMETER") //declaration
+// {
+//     // code
+//     console.log("hi"); //defination
+// } 
+// IdentifierBolteHaiIssko("ARGUMENT") //calling
+
+// Q1. dynamic addition function
+// type => without argument and no return value
+
+// let a=parseInt(prompt("enter a number"))
+// let b=parseInt(prompt("enter a number"))
+// function demo(){
+//     console.log(a+b);   
+// }
+// demo()
+
+// Q2.
+// with argument and no return value
+
+// function demo(a,b){
+//     console.log(a+b);
+// }
+// demo(4,5)
+
+// OR
+
+// function demo(x){
+//     console.log(x+x);
+// }
+// let a=parseInt(prompt("enter number"))
+// add(a)
+
+
+// Q3. WAP to print a table of any number using function type 2 ----> with argument and no return type
+
+// function table(x){
+//     for (let i = 1; i <= 10; i++) {
+//         console.log(i*x);
+//     }
+// }
+// let a=parseInt(prompt("enter number to print table"))
+// table(a)
+
+
+// Q4. WAP using function type 3 ---->without argument and with return value
+
+// function yes(){
+//     return 5
+// }
+// console.log(yes())
+
+
+// Q5. WAP using function type 4 ---->with argument and with return value
+
+function no(a){
+    return a*206
+}
+console.log(no(5));
+
