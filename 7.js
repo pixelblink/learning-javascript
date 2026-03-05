761853884a31e37bd65f2212b34403f01abe2673
pixelblink <chandandhare98@gmail.com>
2026-03-05 10:14:15 +0530

    day7

---
+++ 7.js
@@ -0,0 +1,44 @@
+// =============ARROW FUNCTION=============
+//  2015 ES6
+// arrow functon always returns a value avan when return keyword is not written, return keyword is only written in curly braces{}
+// it is stored in a variable so that it can be called as it does not have any name of its own
+// 
+// Named Function(has a name, it is any function which is not an arrow function)
+// Anonymous Function(does not have a name, it is also knows as arrow function)
+
+// SYNTAX
+
+// ()=>{return }
+
+// ()=>()
+
+// ()=>
+
+// => (thsi one can only take single argument if 2 are needed then use parenthesis)
+
+// =>()
+
+
+
+
+// let fun = ()=>{return "code"}
+// console.log(fun());
+
+
+// let sum=(a,b)=>{return a+b}
+// console.log(sum(6,7));
+
+
+
+// let l = ()=>(2*2)
+// console.log(l());
+
+
+// let u=(a)=>(a+a)
+// console.log(u(5));
+
+
+
+let demo = x => x*x
+console.log(demo(5));
+
