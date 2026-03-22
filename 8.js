336d243ca9fb8e43b63406aae8a6816bf3e92e16
pixelblink <chandandhare98@gmail.com>
2026-03-23 00:05:33 +0530

    day8

--- 8.js
+++ 8.js
@@ -22,7 +22,7 @@ console.log(a6);
 
 
 // ID works only on single elements it targets single element and returns the same
-// class and tag are used to return multiple elements at once  and it returns those multiple element as array 
+// class and tag are used to return multiple elements at once and it returns those multiple element as array 
 
 
 let hi=document.getElementsByClassName('head')
@@ -31,12 +31,16 @@ console.log(hi);
 
 let para=document.getElementsByTagName('p')
 console.log(para);
+
 // changing content of html tag using DOM
+
 // textcontent
+// it just changes the text and if you try to put tags in it, they also get printed as text only
 let h=document.getElementById('new')
 h.textContent="<i> text has been changed </i>"
 
 // innerHTML
+// it targets the wholse line, thus allows you to change the tags alongwith the text within them
 let hh=document.getElementById('newnew')
 hh.innerHTML="<i> text has been changed </i>"
 
