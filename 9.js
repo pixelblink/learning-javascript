bc0cc04e21f85b975d0e59905021db38c1fd87ae
pixelblink <chandandhare98@gmail.com>
2026-03-24 00:21:01 +0530

    day9

---
+++ 9.js
@@ -0,0 +1,20 @@
+// ==================DOM day2==================
+
+document.getElementById('h1').textContent="text has been changes"
+
+let m = document.getElementById('head')
+m.style.backgroundColor="purple"
+m.style.Color="red"
+m.style.padding="200px"
+
+
+let cl = document.getElementsByClassName('head2')
+cl[0].style.backgroundColor="blue"
+
+
+
+
+
+let img = document.getElementById('image')
+img.src="img/1.jpg"
+
