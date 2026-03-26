dfb59d7c0cbbedfcfb6fccb959a075c97c6b0696
pixelblink <chandandhare98@gmail.com>
2026-03-27 00:44:30 +0530

    day12

---
+++ 12.js
@@ -0,0 +1,8 @@
+// setTimeout()
+// it is used to do anything only once within a time duration
+// syntax
+// setTimeout(function,timehandler)
+
+// setTimeout(()=>alert("orr amroddh bhai kaisa"),5000)
+
+setTimeout(()=>{document.getElementById('image').style.display="block"},1000)
