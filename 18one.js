12e5358b0da3b233ffaf7915afa9217294f5ffe3
pixelblink <chandandhare98@gmail.com>
2026-04-02 12:48:16 +0530

    day18

---
+++ 18one.js
@@ -0,0 +1,17 @@
+// JS MODULES
+// two types:- system defined and user defined
+// user defined have two types and they are 
+// 1. Common JS Modules (module.export/module.import(""))  and 
+// 2. ES Modules(export/import)-
+
+function demo(){
+    alert("inside 1st js file")
+}
+export default demo
+
+
+
+
+// default export is used to export single function only   denoted by ''
+// named export lets you export multiple functions   denoted by {}
+
