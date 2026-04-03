7c31358fd02bc89f7b231a375a07e3612891a0c2
pixelblink <chandandhare98@gmail.com>
2026-04-04 00:28:59 +0530

    day20

---
+++ 20.js
@@ -0,0 +1,30 @@
+// ===============API===============
+// Application Programming Interface
+
+// JS is a lightweight programming language which i verry fast
+// syncronous programming language:- runs the code in a sequence regardless of how long the task takes to perform it will complete the task only then move to the next one only
+// asyncronous programming language:- it is FAST af, it runs the code ina way ehere the smallest piece of code runs first ans so on, but it can hinder the
+// JS is both syncronous and asyncronous in nature 
+// whenn fetching API, JS behaves asyncronous
+
+// fetch(URL,{method(http)})
+// method(http) includes--> get(access), post(insert), delete(remove), put and patch(update)
+
+// to fetch API, fetch() is uses with two methods "URL" and "OBJECT"(http) respectively
+
+// URL:- Uniform Resource Locater
+// HTTP:- Hypertext Transfer Protocol
+
+// "https" is secure on the other hand "http" is not safe
+
+// http methods is used to access, insert, remove and update data on brouser which is fetched from API
+
+// =============PROMISE=============
+//  it is a stage of fetch function
+//  types of promise:- reject, pending and fulfill 
+
+// reject:- when error arrives in the process of fetching data from API
+// pending:- when data is not recieved and no errors are being shown while fetching data from API
+// fulfill:- when data is successfully fetched from API
+
+// async and await are promise handlers, they go hand in hand, one will not work without the other one
